91e4ff02e4e6023a7b7cd4dc7841d1a3ba8826b5
Facudezotti <facudezotti@gmail.com>
2026-09-14 16:23:46 -0300

    Usar ganaderia como base predeterminada

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -19,7 +19,7 @@ final class Database
 
         $host = Env::get('DB_HOST', '127.0.0.1');
         $port = Env::get('DB_PORT', '3306');
-        $database = Env::get('DB_DATABASE', 'gestion_ganadera');
+        $database = Env::get('DB_DATABASE', 'ganaderia');
         $username = Env::get('DB_USERNAME', 'root');
         $password = Env::get('DB_PASSWORD', '');
         $dsn = "mysql:host={$host};port={$port};dbname={$database};charset=utf8mb4";
@@ -37,4 +37,3 @@ final class Database
         return self::$connection;
     }
 }
-
